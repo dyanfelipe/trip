9a87ea2860d65383a3dad4f14bfa893e676bae72
Dyan <lumbar_plaice0i@icloud.com>
2023-03-29 20:16:59 -0300

    chore: initial project

---
+++ trip/tripApp.swift
@@ -0,0 +1,17 @@
+//
+//  tripApp.swift
+//  trip
+//
+//  Created by Dyan on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct tripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
